39fbc6df71b97e6f8b85affb7c50d182c1f2f05b
ayush7088 <ayushjoshi282@gmail.com>
2022-12-04 18:56:55 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("script is working from a different  file.")
+// input
+var userName = prompt("Give me your userName")
+// processing
+var welcomeMessage = "this script works ! " + userName
+// output
+alert(welcomeMessage)
