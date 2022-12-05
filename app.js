f77531b8a804281aadfb1e29600a0b7295b23d22
ayush7088 <ayushjoshi282@gmail.com>
2022-12-05 12:00:22 +0530

    done

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 // outputDiv.innerText = "Ayush Joshi"
 
-console.log(txtInput)
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "	https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler() {
-    outputDiv.innerText = "This is :  " + txtInput.value;
+function getTranslationURL(text) {
+    return serverURL + "?" +"text=" + text
+}
+
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with the server. try again after some time.")
+}
+
+function clickHandler() {   
+    var inputText = txtInput.value; // taking input
+
+    //  calling server for processing
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;    // output
+           }   )
+ 
+            .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
